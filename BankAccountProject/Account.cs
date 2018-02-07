82064a99b4d2fa723f565a30697a3c732e0d40cb
TAxford <t.axford2@yahoo.com>
2018-02-07 13:10:49 -0500

    Added account class and checking derived from account

--- BankAccountProject/Account.cs
+++ BankAccountProject/Account.cs
@@ -8,5 +8,36 @@ namespace BankAccountProject
 {
     class Account
     {
+        //fields
+        private bool accountNumber;
+        private double accountBalance;
+
+        //Properties
+        public bool AccountNumber
+        {
+            get { return this.accountNumber; }
+            set { this.accountNumber = value; }
+        }
+
+        public double AccountBalance
+        {
+            get { return this.accountBalance; }
+            set { this.accountBalance = value; }
+        }
+
+        //Constructors
+        public Account()
+        {
+            //Default Constrctor
+        }
+        public Account(bool accountNumber, double accountBalance)
+        {
+            this.accountNumber = accountNumber;
+            this.accountBalance = accountBalance;
+        }
+
+
+
+
     }
 }
---
+++ BankAccountProject/Checking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountProject
+{
+    class Checking : Account
+    {
+        //fields
+        private int accountNumber = 987654321;
+        private double accountBalance = 1000000000;
+
+       
+        public Checking()
+        {
+            //Default Constrctor
+        }
+        public Checking(int accountNumber, double accountBalance)
+        {
+            this.accountNumber = accountNumber;
+            this.accountBalance = accountBalance;
+        }
+    }
+}
