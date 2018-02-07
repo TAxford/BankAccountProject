59ca56edf6bf12d74795553902fa4618059fea53
TAxford <t.axford2@yahoo.com>
2018-02-07 12:04:44 -0500

    Initial Commit

---
+++ BankAccountProject/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountProject
+{
+    class Client
+    {
+        //fields
+        private string firstName;
+        private string lastName;
+        private int age;
+
+    }
+}
