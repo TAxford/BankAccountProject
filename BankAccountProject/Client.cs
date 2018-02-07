165b1b69de45a28be7e2b21b075aaa5d99b1307d
TAxford <t.axford2@yahoo.com>
2018-02-07 12:55:58 -0500

    added client class

--- BankAccountProject/Client.cs
+++ BankAccountProject/Client.cs
@@ -13,5 +13,49 @@ namespace BankAccountProject
         private string lastName;
         private int age;
 
+        //Properties
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set { this.firstName = value; }
+        }
+
+        public string LAstName
+        {
+            get { return this.lastName; }
+            set { this.lastName = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        //Constructors
+        public Client()
+        {
+            //default constructor
+        }
+
+        public Client(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        //Methods
+        public abstract void Information()
+        {
+            Console.WriteLine("Please Eneter your first name.");
+            Console.ReadLine();
+            Console.WriteLine("Please Eneter your last name.");
+            Console.ReadLine();
+            Console.WriteLine("Please eneter your age.");
+            Console.ReadLine();
+        }
+
+
     }
 }
